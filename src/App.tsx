7052f6da02eabcb76429994522ed818fb51cca9b
Natalia <natalia-321s@outlook.com>
2022-09-16 00:20:04 -0300

    Estrutura do modal (com Radix)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
+import * as Dialog from '@radix-ui/react-dialog';
 
 import { GameBanner } from './components/GameBanner';
 import { CreateAdBanner } from './components/CreateAdBanner';
 
 import logoImg from './assets/logo-nlw-esports.svg';
+import { GameController } from 'phosphor-react';
 
 import './styles/main.css';
 
@@ -17,7 +19,7 @@ interface Game {
 }
 
 function App() {
-  const [ games, setGames ] = useState<Game[]>([]);
+  const [games, setGames] = useState<Game[]>([]);
 
   useEffect(() => {
     fetch('http://localhost:3333/games')
@@ -39,7 +41,7 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        { games.map(game => {
+        {games.map(game => {
           return (
             <GameBanner
               bannerUrl={game.bannerUrl}
@@ -48,14 +50,79 @@ function App() {
               key={game.id}
             />
           )
-        }) }
+        })}
 
       </div>
+      <Dialog.Root>
+        <CreateAdBanner />
 
-      <CreateAdBanner />
+        <Dialog.Portal>
+          <Dialog.Overlay className='bg-black/60 inset-0 fixed' />
+
+          <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg'>
+            <Dialog.Title className='text-3xl text-white font-black'>Publique seu anúncio</Dialog.Title>
+
+            <Dialog.Content>
+              <form>
+
+                <div>
+                  <label htmlFor="game">Qual o game?</label>
+                  <input id='game' placeholder='Selecione o game que deseja jogar' />
+                </div>
+
+                <div>
+                  <label htmlFor="name">Qual o game?</label>
+                  <input id='name' placeholder='Como te chamam dentro do game?' />
+                </div>
+
+                <div>
+                  <div>
+                    <label htmlFor="yearsPlaying">Joga há quantos anos?</label>
+                    <input id='yearsPlaying' type="number" placeholder='Tudo bem ser ZERO' />
+                  </div>
+
+                  <div>
+                    <label htmlFor="discord">Qual seu Discord?</label>
+                    <input id='discord' type="text" placeholder='Username#0000' />
+                  </div>
+                </div>
+
+                <div>
+                  <div>
+                    <label htmlFor="weekDays">Quando costuma jogar?</label>
+                  </div>
+
+                  <div>
+                    <label htmlFor="hourStart">Qual horário do dia?</label>
+                    <div>
+                      <input id='hourStart' type="time" placeholder='De' />
+                      <input id='hourEnd' type="time" placeholder='Até' />
+                    </div>
+                  </div>
+                </div>
+
+                <div>
+                  <input type="checkbox" />
+                  Costumo me conectar ao chat de voz
+                </div>
+
+                <footer>
+                  <button>Cancelar</button>
+                  <button type='submit'>
+                    <GameController />
+                    Encontrar Duo
+                  </button>
+                </footer>
+
+              </form>
+            </Dialog.Content>
+          </Dialog.Content>
+
+        </Dialog.Portal>
+      </Dialog.Root>
 
     </div>
   )
 }
 
-export default App
+export default App;
--- src/components/CreateAdBanner.tsx
+++ src/components/CreateAdBanner.tsx
@@ -1,3 +1,4 @@
+import * as Dialog from '@radix-ui/react-dialog';
 import { MagnifyingGlassPlus } from 'phosphor-react';
 
 export function CreateAdBanner(){
@@ -11,10 +12,10 @@ export function CreateAdBanner(){
           <span className='text-zinc-400 block'>Publique seu anúncio para encontrar novos players!</span>
         </div>
 
-        <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 transition text-white rounded flex items-center gap-3'>
+        <Dialog.Trigger className='py-3 px-4 bg-violet-500 hover:bg-violet-600 transition text-white rounded flex items-center gap-3'>
           <MagnifyingGlassPlus size={24}/>
           Publicar anúncio
-        </button>
+        </Dialog.Trigger>
       </div>
     </div>
   )
