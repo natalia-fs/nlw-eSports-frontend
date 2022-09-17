391d1d4d1434331ac0a289a8a7bee529e246acea
Natalia <natalia-321s@outlook.com>
2022-09-17 14:43:34 -0300

    Aula 5 - trilha ignite - Lint + conexão com api

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import logoImg from './assets/logo-nlw-esports.svg';
 
 import './styles/main.css';
 import { CreateAdModal } from './components/CreateAdModal';
+import axios from 'axios';
 
 export interface Game {
   id: string;
@@ -22,10 +23,9 @@ function App() {
   const [games, setGames] = useState<Game[]>([]);
 
   useEffect(() => {
-    fetch('http://localhost:3333/games')
-      .then(response => response.json())
-      .then(data => {
-        setGames(data)
+    axios('http://localhost:3333/games')
+      .then(response => {
+        setGames(response.data)
       })
   }, []);
 
--- src/components/CreateAdModal.tsx
+++ src/components/CreateAdModal.tsx
@@ -1,46 +1,78 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import * as Checkbox from "@radix-ui/react-checkbox";
-import * as ToggleGroup from '@radix-ui/react-toggle-group';
+import * as ToggleGroup from "@radix-ui/react-toggle-group";
 
 import { Check, GameController } from "phosphor-react";
 import { Input } from "./Form/Input";
 import { Game } from "../App";
-import { useEffect, useState } from "react";
+import { useEffect, useState, FormEvent } from "react";
+import axios from "axios";
 
 export function CreateAdModal() {
   const [games, setGames] = useState<Game[]>([]);
   const [weekDays, setWeekDays] = useState<string[]>([]);
-
+  const [useVoiceChannel, setUseVoiceChannel] = useState(false);
+  console.log(weekDays, useVoiceChannel);
   useEffect(() => {
-    fetch('http://localhost:3333/games')
-      .then(response => response.json())
-      .then(data => {
-        setGames(data)
-      })
+    axios("http://localhost:3333/games").then((response) => {
+      setGames(response.data);
+    });
   }, []);
-  
+
+  async function handleCreateAd(event: FormEvent) {
+    event?.preventDefault();
+
+    const formData = new FormData(event.target as HTMLFormElement);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+
+    try {
+      axios.post(`http://localhost:3333/games/${data.game}/ads`, {
+        name: data.name,
+        yearsPlaying: Number(data.yearsPlaying),
+        discord: data.discord,
+        weekDays: weekDays.map(Number),
+        hourStart: data.hourStart,
+        hourEnd: data.hourEnd,
+        useVoiceChannel,
+      });
+      alert("Anúncio criado com sucesso!");
+    } catch (error) {
+      console.error(error);
+      alert("Erro ao criar anúncio!");
+    }
+  }
+
   return (
     <Dialog.Portal>
       <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
 
-      <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg">
+      <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[500px] shadow-lg">
         <Dialog.Title className="text-3xl text-white font-black">
           Publique seu anúncio
         </Dialog.Title>
 
-        <form className="mt-8 flex flex-col gap-4">
+        <form onSubmit={handleCreateAd} className="mt-8 flex flex-col gap-4">
           <div className="flex flex-col gap-2">
             <label className="font-semibold" htmlFor="game">
               Qual o game?
             </label>
             <select
               id="game"
+              name="game"
               className="bg-zinc-900 py-3 px-4 rounded text-small placeholder:zinc-500 appearance-none"
               defaultValue=""
             >
-              <option disabled value=""> Selecione o game que deseja jogar</option>
-              { games.map(game => {
-                return <option value={game.id} key={game.id}>{game.title}</option>
+              <option disabled value="">
+                {" "}
+                Selecione o game que deseja jogar
+              </option>
+              {games.map((game) => {
+                return (
+                  <option value={game.id} key={game.id}>
+                    {game.title}
+                  </option>
+                );
               })}
             </select>
           </div>
@@ -49,6 +81,7 @@ export function CreateAdModal() {
             <label htmlFor="name">Seu nome (ou nickname)</label>
             <Input
               id="name"
+              name="name"
               placeholder="Como te chamam dentro do game?"
               autoComplete="off"
             />
@@ -59,6 +92,7 @@ export function CreateAdModal() {
               <label htmlFor="yearsPlaying">Joga há quantos anos?</label>
               <Input
                 id="yearsPlaying"
+                name="yearsPlaying"
                 type="number"
                 placeholder="Tudo bem ser ZERO"
               />
@@ -66,7 +100,12 @@ export function CreateAdModal() {
 
             <div className="flex flex-col gap-2">
               <label htmlFor="discord">Qual seu Discord?</label>
-              <Input id="discord" type="text" placeholder="Username#0000" />
+              <Input
+                id="discord"
+                name="discord"
+                type="text"
+                placeholder="Username#0000"
+              />
             </div>
           </div>
 
@@ -83,49 +122,77 @@ export function CreateAdModal() {
                 <ToggleGroup.Item
                   value="0"
                   title="Domingo"
-                  className={`w-8 h-8 rounded ${weekDays.includes('0') ? 'bg-violet-500 hover:bg-violet-600' : 'bg-zinc-900 hover:bg-zinc-800'}`}
+                  className={`w-8 h-8 rounded ${
+                    weekDays.includes("0")
+                      ? "bg-violet-500 hover:bg-violet-600"
+                      : "bg-zinc-900 hover:bg-zinc-800"
+                  }`}
                 >
                   D
                 </ToggleGroup.Item>
                 <ToggleGroup.Item
                   value="1"
                   title="Segunda-feira"
-                  className={`w-8 h-8 rounded ${weekDays.includes('1') ? 'bg-violet-500 hover:bg-violet-600' : 'bg-zinc-900 hover:bg-zinc-800'}`}
+                  className={`w-8 h-8 rounded ${
+                    weekDays.includes("1")
+                      ? "bg-violet-500 hover:bg-violet-600"
+                      : "bg-zinc-900 hover:bg-zinc-800"
+                  }`}
                 >
                   S
                 </ToggleGroup.Item>
                 <ToggleGroup.Item
                   value="2"
                   title="Terça-feira"
-                  className={`w-8 h-8 rounded ${weekDays.includes('2') ? 'bg-violet-500 hover:bg-violet-600' : 'bg-zinc-900 hover:bg-zinc-800'}`}
+                  className={`w-8 h-8 rounded ${
+                    weekDays.includes("2")
+                      ? "bg-violet-500 hover:bg-violet-600"
+                      : "bg-zinc-900 hover:bg-zinc-800"
+                  }`}
                 >
                   T
                 </ToggleGroup.Item>
                 <ToggleGroup.Item
                   value="3"
                   title="Quarta-feira"
-                  className={`w-8 h-8 rounded ${weekDays.includes('3') ? 'bg-violet-500 hover:bg-violet-600' : 'bg-zinc-900 hover:bg-zinc-800'}`}
+                  className={`w-8 h-8 rounded ${
+                    weekDays.includes("3")
+                      ? "bg-violet-500 hover:bg-violet-600"
+                      : "bg-zinc-900 hover:bg-zinc-800"
+                  }`}
                 >
                   Q
                 </ToggleGroup.Item>
                 <ToggleGroup.Item
                   value="4"
                   title="Quinta-feira"
-                  className={`w-8 h-8 rounded ${weekDays.includes('4') ? 'bg-violet-500 hover:bg-violet-600' : 'bg-zinc-900 hover:bg-zinc-800'}`}
+                  className={`w-8 h-8 rounded ${
+                    weekDays.includes("4")
+                      ? "bg-violet-500 hover:bg-violet-600"
+                      : "bg-zinc-900 hover:bg-zinc-800"
+                  }`}
                 >
                   Q
                 </ToggleGroup.Item>
                 <ToggleGroup.Item
                   value="5"
                   title="Sexa-feira"
-                  className={`w-8 h-8 rounded ${weekDays.includes('5') ? 'bg-violet-500 hover:bg-violet-600' : 'bg-zinc-900 hover:bg-zinc-800'}`}
+                  className={`w-8 h-8 rounded ${
+                    weekDays.includes("5")
+                      ? "bg-violet-500 hover:bg-violet-600"
+                      : "bg-zinc-900 hover:bg-zinc-800"
+                  }`}
                 >
                   S
                 </ToggleGroup.Item>
                 <ToggleGroup.Item
                   value="6"
                   title="Sábado"
-                  className={`w-8 h-8 rounded ${weekDays.includes('6') ? 'bg-violet-500 hover:bg-violet-600' : 'bg-zinc-900 hover:bg-zinc-800'}`}
+                  className={`w-8 h-8 rounded ${
+                    weekDays.includes("6")
+                      ? "bg-violet-500 hover:bg-violet-600"
+                      : "bg-zinc-900 hover:bg-zinc-800"
+                  }`}
                 >
                   S
                 </ToggleGroup.Item>
@@ -135,14 +202,31 @@ export function CreateAdModal() {
             <div className="flex flex-col gap-2 flex-1">
               <label htmlFor="hourStart">Qual horário do dia?</label>
               <div className="grid grid-cols-2 gap-2">
-                <Input id="hourStart" type="time" placeholder="De" />
-                <Input id="hourEnd" type="time" placeholder="Até" />
+                <Input
+                  name="hourStart"
+                  id="hourStart"
+                  type="time"
+                  placeholder="De"
+                />
+                <Input
+                  name="hourEnd"
+                  id="hourEnd"
+                  type="time"
+                  placeholder="Até"
+                />
               </div>
             </div>
           </div>
 
           <label className="mt-2 flex gap-2 items-center text-small">
-            <Checkbox.Root className="w-6 h-6 rounded bg-zinc-900 flex items-center justify-center">
+            <Checkbox.Root
+              checked={useVoiceChannel}
+              onCheckedChange={(checked: any) => {
+                if (checked === true) setUseVoiceChannel(true);
+                else setUseVoiceChannel(false);
+              }}
+              className="w-6 h-6 rounded bg-zinc-900 flex items-center justify-center"
+            >
               <Checkbox.Indicator className="">
                 <Check className="w-4 h-4 text-emerald-400" />
               </Checkbox.Indicator>
